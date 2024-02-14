e25d423cc1cecd633bc4bf896ac00600c68062b5
jaimehernan95 <jaime.hernan95@gmail.com>
2024-02-14 02:16:59 -0500

    Add javascritp functionality to the buttons

---
+++ JanuaryWebApp/wwwroot/scripts/initSlickSlider.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    // JavaScript for navigation buttons
+    let currentIndex = 0;
+    const slides = $('.carousel .plan-column');
+
+    function showSlide(index) {
+        slides.each(function (i, slide) {
+            if (i === index) {
+                $(slide).show();
+            } else {
+                $(slide).hide();
+            }
+        });
+    }
+
+    function next() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    function prev() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    // Manual navigation
+    $(document).on('click', '.manual-prev', function () {
+        prev();
+    });
+
+    $(document).on('click', '.manual-next', function () {
+        next();
+    });
+
+    // Initially show the first slide
+    showSlide(currentIndex);
+});
